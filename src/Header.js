11a2dfa6f8e52f4da59ac9b3f35e7d50dbb3f926
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-05-25 20:18:12 +0300

    built the left side of the header

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import './Header.css';
+import {
+    BellIcon, 
+    ChatIcon,
+    ChevronDownIcon,
+    HomeIcon,
+    UserGroupIcon,
+    ViewGridIcon,
+} from '@heroicons/react/solid';
 
+import {
+    FlagIcon,
+    PlayIcon,
+    SearchIcon,
+    ShoppinCartIcon,
+} from '@heroicons/react/outline';
+
+ 
 function Header() {
   return ( <div className='header'>
       <div className='header__left'>
           <img src='https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Facebook_f_logo_%282021%29.svg/150px-Facebook_f_logo_%282021%29.svg.png' alt='facebook logo'/>
+        <div className='header__input'>
+            <SearchIcon/>
+            <input type='text' placeholder='Search Facebook'/>
+        </div>
       </div>
 
       <div className='header__middle'></div>
