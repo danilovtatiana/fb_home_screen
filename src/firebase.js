7d0746f219829beda55b10409a2ad72de858a62b
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-05-25 18:48:55 +0300

    firebase configuration

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCHs9yoEXn8davyoSUVwbKDm0XOO9O1y4Q",
+  authDomain: "facebook-home-screen.firebaseapp.com",
+  projectId: "facebook-home-screen",
+  storageBucket: "facebook-home-screen.appspot.com",
+  messagingSenderId: "3519837309",
+  appId: "1:3519837309:web:6f178cd668d423fd9d2d14",
+  measurementId: "G-075YTC1XZM"
+};
