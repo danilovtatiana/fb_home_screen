551c5e678f6afdec5d9deed76f106ea081daefad
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-05-25 19:31:32 +0300

    upload facebook image in left header and set the width

--- src/Header.js
+++ src/Header.js
@@ -3,10 +3,12 @@ import './Header.css';
 
 function Header() {
   return ( <div className='header'>
-      <div className='header__left'></div>
+      <div className='header__left'>
+          <img src='https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Facebook_f_logo_%282021%29.svg/150px-Facebook_f_logo_%282021%29.svg.png' alt='facebook logo'/>
+      </div>
 
       <div className='header__middle'></div>
-      
+
       <div className='header__right'></div>
       
     </div>
